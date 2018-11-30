debcf51be1a394ac32730efd3be0ccdddcd10596
lvjibin <lvjibin@fotoable.com>
2018-11-30 20:57:11 +0800

    add

---
+++ UnityModel/Assets/GameCore/Script/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using VoxelBusters.NativePlugins;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void ShowMessage()
+    {
+        NPBinding.UI.ShowToast("This is a sample message.", eToastMessageLength.SHORT);
+    }
+
+    public void ShowAd()
+    {
+        Advertisement.Show();
+    }
+}
